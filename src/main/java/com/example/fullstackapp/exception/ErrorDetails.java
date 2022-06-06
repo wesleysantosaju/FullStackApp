8948357bd31dd12d2528d547f24de33da90d4dc7
Wesley Santos <wvsantos15@gmail.com>
2022-06-06 14:39:27 -0300

    Implementing ErrorDetails
    Implementing ErrorDetails

--- src/main/java/com/example/fullstackapp/exception/ErrorDetails.java
+++ src/main/java/com/example/fullstackapp/exception/ErrorDetails.java
@@ -1,4 +1,37 @@
 package com.example.fullstackapp.exception;
 
-public class ErrorDetails {
+
+import java.util.Date;
+
+public class ErrorDetails{
+    //CRIANDO ATRIBUTOS
+    private Date timestamp;
+    private String message;
+    private String details;
+
+    //region Bloco - Implementação do Constructor
+    public ErrorDetails(Date timestamp, String message, String details) {
+        super();
+        this.timestamp = timestamp;
+        this.message = message;
+        this.details = details;
+    }
+    //endregion
+
+    //region Bloco - Implementação Getters
+
+    public Date getTimestamp() {
+        return timestamp;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public String getDetails() {
+        return details;
+    }
+    //endregion
+
+
 }
