6225f93a1d20f62943c5bf445bc5b3e0691153d0
Wesley Santos <wvsantos15@gmail.com>
2022-06-06 14:30:17 -0300

    ResourceNotFoundException implementation
    ResourceNotFoundException implementation

--- src/main/java/com/example/fullstackapp/exception/ResourceNotFoundException.java
+++ src/main/java/com/example/fullstackapp/exception/ResourceNotFoundException.java
@@ -1,4 +1,14 @@
 package com.example.fullstackapp.exception;
 
-public class ResourceNotFoundException {
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(value = HttpStatus.NOT_FOUND)
+public class ResourceNotFoundException extends Exception{
+    //CRIANDO SERIAL VERSION
+    private static final long serialVersion = 1L;
+
+    public ResourceNotFoundException(String message){
+        super(message);
+    }
 }
