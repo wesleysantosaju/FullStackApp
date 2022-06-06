00aa0a45d5c3b83ae35de0ff5093e14fa99662b1
Wesley Santos <wvsantos15@gmail.com>
2022-06-06 17:17:01 -0300

    Implementation of components
    Implementation of components, model, service on the front.

--- FullStackFront/full-front-spring/src/app/app-routing.module.ts
+++ FullStackFront/full-front-spring/src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+//importando componentes
+import { EmployeeListComponent } from './componentes/employee-list/employee-list.component';
+import { UpdateEmployeeComponent } from './componentes/update-employee/update-employee.component';
+import { EmployeeDetailsComponent } from './componentes/employee-details/employee-details.component';
+import { CreateEmployeeComponent } from './componentes/create-employee/create-employee.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: 'employee', pathMatch: 'full' },
+  { path: 'employees', component: EmployeeListComponent },
+  { path: 'add', component: CreateEmployeeComponent },
+  { path: 'update/:id', component: UpdateEmployeeComponent },
+  { path: 'details/:id', component: EmployeeDetailsComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- FullStackFront/full-front-spring/src/app/app.module.ts
+++ FullStackFront/full-front-spring/src/app/app.module.ts
@@ -4,15 +4,30 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+//IMPORTANDO OS RECURSOS NECESSÁRIOS
+import { FormsModule } from '@angular/forms';
+import { CreateEmployeeComponent } from './componentes/create-employee/create-employee.component';
+import { EmployeeDetailsComponent } from './componentes/employee-details/employee-details.component';
+import { EmployeeListComponent } from './componentes/employee-list/employee-list.component';
+import { HttpClientModule } from '@angular/common/http';
+import { UpdateEmployeeComponent } from './componentes/update-employee/update-employee.component';
+import { EmployeeService } from './service/employee.service';
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CreateEmployeeComponent,
+    EmployeeDetailsComponent,
+    EmployeeListComponent,
+    UpdateEmployeeComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule
   ],
-  providers: [],
+  providers: [EmployeeService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
