1c4b144deb22914e7b836fbfa303564b3e64e668
Wesley Santos <wvsantos15@gmail.com>
2022-06-06 12:01:29 -0300

    Alteração/Adição de recursos na EmployeeController
    Creation of dependency injection in the Controller Employee class, created method for listing users.

--- src/main/java/com/example/fullstackapp/controllers/EmployeeController.java
+++ src/main/java/com/example/fullstackapp/controllers/EmployeeController.java
@@ -1,4 +1,35 @@
 package com.example.fullstackapp.controllers;
 
+import com.example.fullstackapp.model.Employee;
+import com.example.fullstackapp.repository.EmployeeRepository;
+import com.example.fullstackapp.service.SequenceGeneratorService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+//@CrossOrigin(origins = "http://localhost:4200")
+@RestController
+@RequestMapping("/api/v1")
 public class EmployeeController {
+
+    //region Bloco - Injeções de Dependências
+    //APLICANDO AS INJEÇÕES DE DEPENDÊNCIA
+    @Autowired
+    private EmployeeRepository employeeRepository;
+    @Autowired
+    private SequenceGeneratorService sequenceGeneratorService;
+    //endregion
+
+    //region Bloco - Requisições HTTP
+    //criar método para trazer todos os registros encontrados no DB
+    @GetMapping
+    public List<Employee> getAllEmployees(){
+        return employeeRepository.findAll();
+    }
+    //enregion
+
 }
