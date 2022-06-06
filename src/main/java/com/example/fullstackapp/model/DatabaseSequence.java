0406b30cd2603d9888c4f4c8d85713363868e48a
Wesley Santos <wvsantos15@gmail.com>
2022-06-06 13:16:59 -0300

    Database model implementation
    Database model implementation

--- src/main/java/com/example/fullstackapp/model/DatabaseSequence.java
+++ src/main/java/com/example/fullstackapp/model/DatabaseSequence.java
@@ -5,11 +5,12 @@ import org.springframework.data.mongodb.core.mapping.Document;
 
 @Document(collection = "database_sequences")
 public class DatabaseSequence {
+    //DATABASE SEQUENCE
     @Id
     private String id;
     private long seq;
 
-    //region Bloco - Getters and Setters
+    //region Bloco - Implementação dos Getters and Setters
     public String getId() {
         return id;
     }
